86d12de5cbab691270d7e6f44bdb12ec973e78f4
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2017-10-16 22:20:39 +0800

    made button code (not tested on phone cuz cannot open show story activity yet), sharing function added to show story, used own edited images for buttons

--- app/src/main/java/com/projectbored/app/ShowStory.java
+++ app/src/main/java/com/projectbored/app/ShowStory.java
@@ -1,30 +1,68 @@
 package com.projectbored.app;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class ShowStory extends AppCompatActivity {
+public class ShowStory extends ActionBarActivity implements View.OnClickListener {
 
-    Button upVoteButton;
-    Button downVoteButton;
+    ImageButton upVoteButton;
+    ImageButton downVoteButton;
+    ImageButton shareButton;
+
+    // onCreate method here -hy
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_story);
 
-        Button upVoteButton = (Button)findViewById(R.id.upVoteButton);
-        
-        //tbc
+        ImageButton mClickUpVoteButton = (ImageButton)findViewById(R.id.upVoteButton);
+        mClickUpVoteButton.setOnClickListener(this);
+        ImageButton mClickDownVoteButton = (ImageButton)findViewById(R.id.downVoteButton);
+        mClickDownVoteButton.setOnClickListener(this);
+        ImageButton mClickShare = (ImageButton)findViewById(R.id.shareButton);
+        mClickShare.setOnClickListener(this);
 
         }
     }
 
-    // creating the menu that launches backToMap method
+    // stuff the buttons do when clicked -hy
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.upVoteButton: {
+                // do something for button 1 click
+                break;
+            }
+
+            case R.id.downVoteButton: {
+                // do something for button 2 click
+                break;
+            }
+
+            case R.id.shareButton: {
+                // this is the sharing code, it might not work -hy
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                // i don't actually know what the subject or whatnot is so heh
+                String shareBody = "Check out this cool story on bored!";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Cool BORED! story");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                break;
+            }
+        }
+    }
+
+
+    // creating the menu that launches backToMap method -hy
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,12 +78,12 @@ public class ShowStory extends AppCompatActivity {
         return true;
     }
 
-    // backToMap method (that just goes back to map i guess lol)
-    // idk if this works
+    // backToMap method (that just goes back to map i guess lol) -hy
+    // idk if this works -hy
 
     private void backToMap() {
-        // to fill in with code
-        // i think this is suppose to open the map activity
+        // to fill in with code -hy
+        // i think this is suppose to open the map activity -hy
         Intent intent = new Intent(this, MapsActivityCurrentPlace.class);
         startActivity(intent);
     }
