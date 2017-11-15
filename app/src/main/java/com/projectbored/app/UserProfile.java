60bc1319308e1e1ec785946cf49cf1e37ac922a9
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2017-11-15 22:04:46 +0800

    user xml with icons uploaded

--- app/src/main/java/com/projectbored/app/UserProfile.java
+++ app/src/main/java/com/projectbored/app/UserProfile.java
@@ -2,12 +2,16 @@ package com.projectbored.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class UserProfile extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
     }
+
+
 }
