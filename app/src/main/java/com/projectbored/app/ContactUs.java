d3c59e85028af877d634a85151e19cb2a6648c33
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2017-12-31 18:05:41 +0800

    Working Basic Contact Us Form (Intro, Subject, Message)

--- app/src/main/java/com/projectbored/app/ContactUs.java
+++ app/src/main/java/com/projectbored/app/ContactUs.java
@@ -3,8 +3,19 @@ package com.projectbored.app;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ContactUs extends AppCompatActivity {
+
+    EditText textSubject;
+    EditText textMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +26,38 @@ public class ContactUs extends AppCompatActivity {
         actionBar.setLogo(R.drawable.whitebored);
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        textSubject = findViewById(R.id.subject_input);
+        textMessage = findViewById(R.id.message_input);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.contact_us_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.option_send_message) {
+
+            String to = "projectbored.inc@gmail.com";
+            String subject = textSubject.getText().toString();
+            String message = textMessage.getText().toString();
+
+            Intent email = new Intent(Intent.ACTION_SEND);
+            email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+            //email.putExtra(Intent.EXTRA_CC, new String[]{ to});
+            //email.putExtra(Intent.EXTRA_BCC, new String[]{to});
+            email.putExtra(Intent.EXTRA_SUBJECT, subject);
+            email.putExtra(Intent.EXTRA_TEXT, message);
+
+            //need this to prompts email client only
+            email.setType("message/rfc822");
+
+            startActivity(Intent.createChooser(email, "Send Email with..."));
+
+        }
+        return true;
     }
 }
