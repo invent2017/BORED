b4c2e31847d6eded755c61fe775c161a99545aaa
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-04-03 22:02:17 +0800

    more multisquawk code: readgeotag method integrated to uploadimage and finally not crashing , issues with fetching exif data?

--- app/src/main/java/com/projectbored/app/MultiSquawk.java
+++ app/src/main/java/com/projectbored/app/MultiSquawk.java
@@ -44,6 +44,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;import android.annotation.SuppressLint;
+import android.location.Location;
+import android.media.ExifInterface;
+
 
 public class MultiSquawk extends AppCompatActivity {
 
@@ -151,6 +158,7 @@ public class MultiSquawk extends AppCompatActivity {
             Uri photoUri = FileProvider.getUriForFile(this, "com.projectbored.app.fileprovider", photoFile);
             photoPickerIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
             startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+            storySettings.putString("PHOTO_URI",photoUri.getPath());
         }
     }
 
@@ -242,12 +250,10 @@ public class MultiSquawk extends AppCompatActivity {
     }
 
     private void uploadStoryData () {
-        final Location storyLocation = new Location(LocationManager.GPS_PROVIDER);
-        storyLocation.setLatitude(storySettings.getDouble("Latitude"));
-        storyLocation.setLongitude(storySettings.getDouble("Longitude"));
+        String photoUri = storySettings.getString("PHOTO_URI");
+        final Location storyLocation = new Location(readGeoTagImage(photoUri));
 
         if (storyLocation != null) {
-            //TODO: add location info things from exif (can ref https://developer.android.com/reference/android/media/ExifInterface.html)
             final String locationString = Double.toString(storyLocation.getLatitude())
                     + ","
                     + Double.toString(storyLocation.getLongitude());
@@ -357,77 +363,27 @@ public class MultiSquawk extends AppCompatActivity {
         });
     }
 
-
-    public class geoDegree {
-        private boolean valid = false;
-        Float Latitude, Longitude;
-        geoDegree(ExifInterface exif){
-            String attrLATITUDE = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
-            String attrLATITUDE_REF = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
-            String attrLONGITUDE = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
-            String attrLONGITUDE_REF = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
-
-            if((attrLATITUDE !=null)
-                    && (attrLATITUDE_REF !=null)
-                    && (attrLONGITUDE != null)
-                    && (attrLONGITUDE_REF !=null))
-            {
-                valid = true;
-
-                if(attrLATITUDE_REF.equals("N")){
-                    Latitude = convertToDegree(attrLATITUDE);
-                }
-                else{
-                    Latitude = 0 - convertToDegree(attrLATITUDE);
-                }
-
-                if(attrLONGITUDE_REF.equals("E")){
-                    Longitude = convertToDegree(attrLONGITUDE);
-                }
-                else{
-                    Longitude = 0 - convertToDegree(attrLONGITUDE);
-                }
-
+    /**
+     * Read location information from image.
+     * @param imagePath : image absolute path
+     * @return : location information
+     */
+    public Location readGeoTagImage(String imagePath)
+    {
+        Location loc = new Location("");
+        try {
+            ExifInterface exif = new ExifInterface(imagePath);
+            float [] latlong = new float[2] ;
+            if(exif.getLatLong(latlong)){
+                loc.setLatitude(latlong[0]);
+                loc.setLongitude(latlong[1]);
             }
-        };
 
-        final public String gpsString = String.valueOf(Latitude) + ", " + String.valueOf(Longitude);
-
-        private Float convertToDegree(String stringDMS){
-            Float result = null;
-            String[] DMS = stringDMS.split(",", 3);
-
-            String[] stringD = DMS[0].split("/", 2);
-            Double D0 = new Double(stringD[0]);
-            Double D1 = new Double(stringD[1]);
-            Double FloatD = D0/D1;
-
-            String[] stringM = DMS[1].split("/", 2);
-            Double M0 = new Double(stringM[0]);
-            Double M1 = new Double(stringM[1]);
-            Double FloatM = M0/M1;
-
-            String[] stringS = DMS[2].split("/", 2);
-            Double S0 = new Double(stringS[0]);
-            Double S1 = new Double(stringS[1]);
-            Double FloatS = S0/S1;
-
-            result = new Float(FloatD + (FloatM/60) + (FloatS/3600));
-
-            return result;
-
-        };
-
-        public int getLatitudeE6(){
-            return (int)(Latitude*1000000);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        public int getLongitudeE6(){
-            return (int)(Longitude*1000000);
-        }
-
+        return loc;
     }
-
 
 
 }
